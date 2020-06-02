dc7ac670076fe7ed4707dd43a6a7eeadf17826d3
Emmanuel <tholuzi@gmail.com>
2020-06-03 00:03:47 +0100

    update

---
+++ fake.js
@@ -0,0 +1,12 @@
+/*
+    fake api json for software developers~
+    1. User Sign Up and Login
+    2. Article -> with basic functions
+    3. Location apiS
+    4. Create Profile -> with basic functionalities
+    5. Delete Account
+    6. Password Reset
+    7. Forgot Password
+    8. Get User ->  basic routes
+    9.
+ */
